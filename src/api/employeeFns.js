6c9c9294a8b7d95706c4e5fc5f4be45e0a4d6d10
Brad White <bradscottwhite@gmail.com>
2022-11-01 09:46:50 -0400

    added axios

--- src/api/employeeFns.js
+++ src/api/employeeFns.js
@@ -1,12 +1,15 @@
 /*
  * Employee REST api functions
  */
+import axios from 'axios'
 
-const url = 'http://springawsdemo1-env.eba-rquu33pm.us-east-1.elasticbeanstalk.com/employee/'
+const url = 'http://springawsdemo2-env.eba-gpepswai.us-east-1.elasticbeanstalk.com/employee/'
 
 export const getEmployee = async id => {
-	const res = await fetch(url + id)
-	return await res.json()
+	//const res = await fetch(url + id)
+	//return await res.json()
+	const res = await axios.get(url + id)
+	return await res.data
 }
 
 ;
